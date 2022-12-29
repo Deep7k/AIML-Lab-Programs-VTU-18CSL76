38efc02c550a658db9d931aa8d443dcbc597b011
Deepak Nalathwad <60418902+Deep7k@users.noreply.github.com>
2022-12-29 12:45:48 +0530

    Add question

--- programs/KMeans.py
+++ programs/KMeans.py
@@ -1,3 +1,7 @@
+"""Apply EM algorithm to cluster a set of data stored in a .CSV file. Use the same data set for clustering using
+k-Means algorithm. Compare the results of these two algorithms and comment on the quality of clustering. You can add
+Java/Python ML library classes/API in the program """
+
 import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.cluster import KMeans
